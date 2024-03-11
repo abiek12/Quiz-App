aba7ddf88350106e37ba23564100fd6ca382fa00
Abhishek pp <abhishekkanichery@gmail.com>
2024-03-11 12:55:32 +0530

    cretes user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { Document, Schema, Model } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("users", userSchema);
